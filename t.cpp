daf768e69b3e7c1a6998d6c5a7f2bfc4ef000dc7
Edward Kmett <ekmett@gmail.com>
2021-01-16 19:40:33 -0800

    factor out dl

--- t.cpp
+++ t.cpp
@@ -5,11 +5,16 @@
 #include <dlfcn.h>
 using namespace autodiff;
 
+struct dl {
+  dl(const char * filename = nullptr) : handle(dlopen(filename,0)) {}
+  ~dl() { dlclose(handle); }
+  void * handle;
+};
+
 // i have one off by one error and its infuriating.
 int main (int argc, char ** argv) {
-  void *handle = dlopen(nullptr, 0);
+  dl open;
   auto result = diff([](auto x) { auto y = x*x; auto z = y*y; auto w = z*z; return w*w; }, 2);
   std::cout << "It lives: " << std::get<0>(result) << ", " << std::get<1>(result) << std::endl;
-  dlclose(handle);
   return 0;
 }
