7fb3a03b8769010a00a6fce8c5d60ffafe69580f
Edward Kmett <ekmett@gmail.com>
2021-01-19 22:36:54 -0800

    make dl bad

--- dl.hh
+++ dl.hh
@@ -1,20 +1,23 @@
 #pragma once
 #include <dlfcn.h>
 
-// convenient RAII wrapper for dlopening, usually used for the current program
-struct dl {
-  dl(const char * filename = nullptr) : handle(dlopen(filename,0)) {}
-  ~dl() { dlclose(handle); }
-  void * handle;
-};
+namespace bad {
 
-// identify a pointer to some built-in thing described at compile time.
-template <class T>
-std::string wat(T * t) {
-  Dl_info info;
-  if (dladdr(reinterpret_cast<const void *>(t), &info)) {
-    return info.dli_sname;
-  } else {
-    return std::to_string(reinterpret_cast<intptr_t>(t));
+  // convenient RAII wrapper for dlopening, usually used for the current program
+  struct dl {
+    dl(const char * filename = nullptr) : handle(dlopen(filename,0)) {}
+    ~dl() { dlclose(handle); }
+    void * handle;
+  };
+
+  // identify a pointer to some built-in thing described at compile time.
+  template <class T>
+  std::string wat(T * t) {
+    Dl_info info;
+    if (dladdr(reinterpret_cast<const void *>(t), &info)) {
+      return info.dli_sname;
+    } else {
+      return std::to_string(reinterpret_cast<intptr_t>(t));
+    }
   }
 }
