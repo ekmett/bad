98e57e937704755d57fbf7bb8cbdea2e56382643
Edward Kmett <ekmett@gmail.com>
2021-01-19 18:28:51 -0800

    cleanup

--- t_seq.cc
+++ t_seq.cc
@@ -3,21 +3,20 @@
 #include "seq.hh"
 
 using namespace bad;
-using namespace std;
 
-TEST_CASE( "seq_head works", "[shape]" ) {
-   REQUIRE( seq_head<seq<1,2,3>> == 1 );
-   REQUIRE( seq_head<std::integer_sequence<std::size_t,4,2,1> > == 4 );
+TEST_CASE("seq_head works", "[shape]") {
+  REQUIRE(seq_head<seq<1,2,3>> == 1 );
+  REQUIRE(seq_head<seq_t<size_t,4,2,1> > == 4 );
 }
 
 TEST_CASE("stride works","[shape]") {
-  REQUIRE( stride<0,int,7,5,3> == 15);
-  REQUIRE( stride<1,int,7,5,3> == 3);
-  REQUIRE( stride<2,int,7,5,3> == 1);
+  REQUIRE(stride<0,int,7,5,3> == 15);
+  REQUIRE(stride<1,int,7,5,3> == 3);
+  REQUIRE(stride<2,int,7,5,3> == 1);
 }
 
 TEST_CASE("prod works","[shape]") {
-  REQUIRE( prod<4,2,3> == 24 );
+  REQUIRE(prod<4,2,3> == 24 );
 }
 
 TEST_CASE("nth works","[shape]") {
@@ -25,9 +24,9 @@ TEST_CASE("nth works","[shape]") {
 }
 
 TEST_CASE("seq_nth works","[shape]") {
-   REQUIRE(seq_nth<4,seq<0,11,21,31,41,51>> == 41);
+  REQUIRE(seq_nth<4,seq<0,11,21,31,41,51>> == 41);
 }
 TEST_CASE("seq_length works","[shape]") {
-   REQUIRE(seq_length<seq<3,2,1>> == 3);
+  REQUIRE(seq_length<seq<3,2,1>> == 3);
 }
 
