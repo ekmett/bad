3dac65505e8e79a3ee024e356203318d0e72019f
Edward Kmett <ekmett@gmail.com>
2021-01-21 17:56:24 -0800

    we're assuming c++17

--- attrib.hh
+++ attrib.hh
@@ -1,15 +1,27 @@
 #pragma once
 #include "map.hh"
 
-#ifndef __has_attribute
-#define __has_attribute(__x) 0
+#ifdef __has_attribute
+#define bad_has_attribute(__x) __has_attribute(__x)
+#else
+#define bad_has_attribute(__x) 0
 #endif
 
-#ifndef __has_declspec_attribute
-#define __has_declspec_attribute(__x) 0
+#if defined(_MSC_VER)
+#define bad_has_declspec_attribute(__x) 1
+#elif defined(BAD_USE_DECLSPEC) && defined(__has_declspec_attribute)
+#define bad_has_declspec_attribute(__x) __has_declspec_attribute(__x)
+#else
+#define bad_has_declspec_attribute(__x) 0
 #endif
 
-#if __has_cpp_attribute(likely)
+#ifdef __has_cpp_attribute
+#define bad_has_cpp_attribute(x) __has_cpp_attribute(x)
+#else
+#define bad_has_cpp_attribute(x) 0
+#endif
+
+#if bad_has_cpp_attribute(likely)
 #define bad_likely(x) (x) [[likely]]
 #define bad_unlikely(x) (x) [[unlikely]]
 #else
@@ -20,7 +32,7 @@
 // TODO: restrict
 // TODO: gsl::Owner, gsl::Pointer?
 
-#if __has_attribute(always_inline)
+#if bad_has_attribute(always_inline)
 #define bad_inline inline __attribute__((always_inline))
 #elif defined _WIN32
 #define bad_inline __forceinline
@@ -28,20 +40,15 @@
 #define bad_inline inline
 #endif
 
-#if __has_attribute(flatten)
+#if bad_has_attribute(flatten)
 #define bad_flatten __attribute__((flatten))
 #else
 #define bad_flatten
 #warning no flatten
 #endif
 
-// #if __clang__
-// #define bad_no_unique_address [[no_unique_address]]
-// #else
-#define bad_no_unique_address
-// #endif
 
-#if __has_attribute(malloc)
+#if bad_has_attribute(malloc)
 #define bad_malloc __attribute__((malloc))
 #else
 #define bad_malloc
@@ -49,7 +56,7 @@
 #endif
 
 
-#if __has_attribute(uninitialized)
+#if bad_has_attribute(uninitialized)
 #define bad_uninitialized __attribute__((uninitialized))
 #else
 #define bad_uninitialized
@@ -58,7 +65,7 @@
 
 
 /// only modifies data reachable through pointer arguments
-#if BAD_USE_DECLSPEC && __has_declspec_attribute(noalias)
+#if bad_has_declspec_attribute(noalias)
 #define bad_noalias __declspec(noalias)
 #else
 #define bad_noalias
@@ -66,7 +73,7 @@
 #endif
 
 /// only modifies data reachable through pointer arguments
-#if __has_attribute(require_constant_initialization)
+#if bad_has_attribute(require_constant_initialization)
 #define bad_constinit __attribute__((require_constant_initialization))
 #else
 #define bad_constinit
@@ -76,7 +83,7 @@
 /// check that this is called once on all execution paths
 
 /// check that this is called once on all execution paths
-//#if __has_attribute(called_once)
+//#if bad_has_attribute(called_once)
 //#define BAD_CALLED_ONCE __attribute__((called_once))
 //#else
 //#define BAD_CALLED_ONCE
@@ -86,7 +93,7 @@
 /// no effect other than return value, may inspect globals
 
 /// only examines arguments, no effect other than return value
-#if __has_attribute(const)
+#if bad_has_attribute(const)
 #define bad_const __attribute__((const))
 #else
 #define bad_const
@@ -94,7 +101,7 @@
 #endif
 
 /// no effect other than return value, may inspect globals
-#if __has_attribute(pure)
+#if bad_has_attribute(pure)
 #define bad_pure __attribute__((pure))
 #else
 #define bad_pure
@@ -111,7 +118,7 @@
 
 // C++14
 
-#if __has_attribute(assume_aligned)
+#if bad_has_attribute(assume_aligned)
 #define bad_assume_aligned(x) __attribute__((assume_aligned(x)))
 #else
 #define bad_assume_aligned(x)
@@ -120,7 +127,7 @@
 
 // arg # (1-based) of the attribute that tells you the alignment of the result
 
-#if __has_attribute(align_value)
+#if bad_has_attribute(align_value)
 #define bad_align_value(x) __attribute__((align_value(x)))
 #else
 #define bad_align_value(x)
@@ -128,7 +135,7 @@
 #endif
 
 // arg # (1-based) of the attribute that tells you the alignment of the result
-#if __has_attribute(alloc_align)
+#if bad_has_attribute(alloc_align)
 #define bad_alloc_align(x) __attribute__((alloc_align(x)))
 #else
 #define bad_alloc_align(x)
@@ -136,28 +143,28 @@
 #endif
 
 // arg # (1-based) of the attribute that tells you the size of the result in bytes
-#if __has_attribute(alloc_size)
+#if bad_has_attribute(alloc_size)
 #define bad_alloc_size(x) __attribute__((alloc_size(x)))
 #else
 #define bad_alloc_size(x)
 #warning no alloc_size
 #endif
 
-#if __has_attribute(noescape)
+#if bad_has_attribute(noescape)
 #define bad_noescape __attribute__((noescape))
 #else
 #define bad_noescape
 #warning no noescape
 #endif
 
-#if __has_attribute(callback)
+#if bad_has_attribute(callback)
 #define bad_callback(...) __attribute__((callback(__VA_ARGS__)))
 #else
 #define bad_callback(...)
 #warning no callback
 #endif
 
-#if __has_attribute(lifetimebound)
+#if bad_has_attribute(lifetimebound)
 #define bad_lifetimebound __attribute__((lifetimebound))
 #else
 #define bad_lifetimebound
@@ -165,7 +172,7 @@
 #endif
 
 
-#if __has_attribute(returns_nonnull)
+#if bad_has_attribute(returns_nonnull)
 #define bad_returns_nonnull __attribute__((returns_nonnull))
 #else
 #define bad_returns_nonnull
@@ -184,19 +191,18 @@
 
 #define bad_hd bad_host bad_device
 
+/// @since C++20
+#if bad_has_cpp_attribute(no_unique_address)
+#define bad_no_unique_address [[no_unique_address]]
+#else
+#define bad_no_unique_address
+#endif
+
 /// @since C++17
 #define bad_maybe_unused [[maybe_unused]]
 
 /// @since C++14
-#if __has_cpp_attribute(deprecated)
 #define bad_deprecated [[deprecated]]
-#else
-#warning no deprecated
-#endif
 
 /// @since C++17
-#if __has_cpp_attribute(nodiscard)
 #define bad_nodiscard [[nodiscard]]
-#else
-#warning no nodiscard
-#endif
